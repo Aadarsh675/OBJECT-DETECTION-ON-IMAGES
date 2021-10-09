294d592156203133ea97f239553089a5c5c281ee
Aadarsh675 <76542875+Aadarsh675@users.noreply.github.com>
2021-10-09 10:00:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom() {
+    window.location = "bedroom.html";
+}
+function tvandac() {
+    window.location = "tvandac.html";
+}
+function desk() {
+    window.location = "desk.html";
+}
+function bottles() {
+    window.location = "bottles.html";
+}
+function fruitbasket() {
+    window.location = "fruitbasket.html";
+}
